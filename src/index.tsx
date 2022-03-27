f7f804f29d1569d48d7be1a101bfa9eed140446e
Yujia Wu <ywu660@aucklanduni.ac.nz>
2022-03-27 12:27:54 +0800

    Set up the Ionic framework inside the react project.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
